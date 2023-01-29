61ca2d0190680393a2e568bc0a4019e10d724523
Ahmad <ahmadghadban02@gmail.com>
2023-01-29 17:24:16 -0500

    fixed

---
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IPropertyBase } from 'src/app/model/ipropertybase';
+import { HousingService } from 'src/app/services/housing.service';
+
+
+@Component({
+  selector: 'app-property-list',
+  templateUrl: './property-list.component.html',
+  styleUrls: ['./property-list.component.css']
+})
+export class PropertyListComponent implements OnInit {
+
+  SellRent = 1;
+  properties: Array<IPropertyBase>;
+  Today = new Date();
+  City = '';
+  SearchCity = '';
+  SortbyParam = '';
+  order = 'asc';
+
+  constructor(private route: ActivatedRoute, private housingService: HousingService) {
+   }
+  ngOnInit(): void {
+    if (this.route.snapshot.url.toString()) {
+      this.SellRent = 2;
+    }
+    this.housingService.getAllProperties(this.SellRent).subscribe(
+        data=>{
+          this.properties = data;
+
+
+
+          console.log(data)
+
+        }, error => {
+          console.log('httperror:');
+          console.log(error);
+        }
+    );
+  }
+
+  onCityFilter() {
+    this.SearchCity = this.City;
+  }
+
+  onCityFilterClear() {
+    this.SearchCity = '';
+    this.City = '';
+  }
+
+  onOrder() {
+    if (this.order === 'asc'){
+      this.order = 'desc';
+    } else if (this.order === 'desc'){
+      this.order = 'asc';
+    }
+  }
+}
---
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/model/user';
+import { AlertifyService } from 'src/app/services/alertify.service';
+
+
+@Component({
+  selector: 'app-user-register',
+  templateUrl: './user-register.component.html',
+  styleUrls: ['./user-register.component.css']
+})
+export class UserRegisterComponent implements OnInit {
+
+  registerationForm!: FormGroup;
+  user: User;
+  userSubmitted: boolean;
+  constructor(private fb: FormBuilder, private userService: UserService, private alertify: AlertifyService) { }
+
+  ngOnInit() {
+    // this.registerationForm = new FormGroup({
+    //   userName: new FormControl(null, Validators.required),
+    //   email: new FormControl(null, [Validators.required, Validators.email]),
+    //   password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    //   confirmPassword: new FormControl(null, [Validators.required]),
+    //   mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
+    // }, this.passwordMatchingValidator);
+    // this.registerationForm.controls['userName'].setValue('Default Value');
+    this.createRegisterationForm();
+  }
+
+  createRegisterationForm() {
+    this.registerationForm = this.fb.group({
+      userName: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+      confirmPassword: [null, Validators.required],
+      mobile: [null, [Validators.required, Validators.maxLength(10)]]
+    }, {validators: this.passwordMatchingValidator});
+  }
+
+  passwordMatchingValidator(fc: AbstractControl): ValidationErrors | null {
+    return fc.get('password')?.value === fc.get('confirmPassword')?.value ? null : {notmatched: true};
+  }
+
+  get userName() {
+    return this.registerationForm.get('userName') as FormControl;
+  }
+
+  get email() {
+    return this.registerationForm.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.registerationForm.get('password') as FormControl;
+  }
+
+  get confirmPassword() {
+    return this.registerationForm.get('confirmPassword') as FormControl;
+  }
+
+  get mobile() {
+    return this.registerationForm.get('mobile') as FormControl;
+  }
+
+  onSubmit() {
+    console.log(this.registerationForm);
+    this.userSubmitted = true;
+    if (this.registerationForm.valid){
+      // this.user = Object.assign(this.user, this.registerationForm.value);
+      this.userService.addUser(this.userData());
+      this.registerationForm.reset();
+      this.userSubmitted = false;
+      this.alertify.success('Congrats, you are successfylly registered');
+    } else {
+      this.alertify.error("Kindly provide the required fields");
+
+    }
+  }
+
+  userData(): User {
+    return this.user = {
+      userName: this.userName.value,
+      email: this.email.value,
+      password: this.password.value,
+      mobile: this.mobile.value
+    }
+  }
+
+}
