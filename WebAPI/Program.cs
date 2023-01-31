06b0bdcc3bfdb674854f0a2b784ed9e009beb17e
Ahmad <ahmadghadban02@gmail.com>
2023-01-31 09:18:18 -0500

    fixed errors

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+ 
 var app = builder.Build();
 var env = app.Environment;
 // Configure the HTTP request pipeline.
