7e5a0e714897924c9a56c7cdd786b777cfd8e60c
Ahmad <ahmadghadban02@gmail.com>
2023-01-31 14:36:51 -0500

    match password hash added

--- WebAPI/Interfaces/IUserRepository.cs
+++ WebAPI/Interfaces/IUserRepository.cs
@@ -5,5 +5,9 @@ namespace WebAPI.Interfaces
     public interface IUserRepository
     {
         Task<User> Authenticate(string username, string password);
+
+        void Register(string username, string password);
+
+        Task<bool> UserAlreadyExists(string username);
     }
 }
